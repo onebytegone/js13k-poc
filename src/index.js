a164a0e30cb2aa78bbc849dc219b06935d64387d
Ethan Smith <ethan@onebytegone.com>
2020-08-15 22:13:59 -0400

    feat: proc gen map

--- src/index.js
+++ src/index.js
@@ -132,3 +132,126 @@ AFRAME.registerComponent('touch-controls', {
 document.addEventListener('keypress', function(e) {
    document.getElementById('camera').components['wasd-controls'].data.acceleration = e.shiftKey ? 220 : 65;
 });
+
+function makeWallEl(width) {
+   const el = document.createElement('a-box');
+
+   el.setAttribute('width', width);
+   el.setAttribute('height', '4');
+   el.setAttribute('depth', '0.25');
+   el.setAttribute('color', '#ededea');
+   return el;
+}
+
+function createWall(mapX, mapY, dir, style) {
+   if (style === 'gap') {
+      return;
+   }
+
+   const sceneEl = document.querySelector('a-scene');
+
+   let cellX = 0,
+       cellZ = 0;
+
+   if (dir === 0) {
+      cellX = 0;
+      cellZ = -4;
+   } else if (dir === 1) {
+      cellX = 4;
+      cellZ = 0;
+   } else if (dir === 2) {
+      cellX = 0;
+      cellZ = 4;
+   } else {
+      cellX = -4;
+      cellZ = 0;
+   }
+
+   if (style === 'door') {
+      const sideA = makeWallEl('2.5');
+            sideB = makeWallEl('2.5');
+
+      sideA.setAttribute('position', `${(mapX * 8) + cellX + ((dir + 1) % 2 * 2.75)} 2 ${(mapY * 8) + cellZ + (dir % 2 * 2.75)}`);
+      sideA.setAttribute('rotation', `0 ${dir % 2 * 90} 0`);
+      sceneEl.appendChild(sideA);
+
+      sideB.setAttribute('position', `${(mapX * 8) + cellX + ((dir + 1) % 2 * -2.75)} 2 ${(mapY * 8) + cellZ + (dir % 2 * -2.75)}`);
+      sideB.setAttribute('rotation', `0 ${dir % 2 * 90} 0`);
+      sceneEl.appendChild(sideB);
+   } else {
+      const el = makeWallEl('8');
+
+      el.setAttribute('position', `${(mapX * 8) + cellX} 2 ${(mapY * 8) + cellZ}`);
+      el.setAttribute('rotation', `0 ${dir % 2 * 90} 0`);
+      sceneEl.appendChild(el);
+   }
+}
+
+function createFloor(mapW, mapH) {
+   const el = document.createElement('a-plane'),
+         sceneEl = document.querySelector('a-scene'),
+         w = mapW * 8,
+         h = mapH * 8;
+
+   el.setAttribute('position', `${w / 2 - 4} 0 ${h / 2 - 4}`);
+   el.setAttribute('width', w);
+   el.setAttribute('height', h);
+   el.setAttribute('color', '#efa0e0');
+   el.setAttribute('rotation', '-90 0 0');
+
+   sceneEl.appendChild(el);
+}
+
+const PRNG = function(seed) {
+   return {
+      next: function() {
+         seed = (seed * 9301 + 49297) % 233280;
+         return seed / 233280;
+      },
+      between: function(min, max, asInt) {
+         const val = min + this.next(asInt) * (max - min);
+
+         return asInt ? Math.round(val) : val;
+      },
+   };
+};
+
+function generateMap(prng, width, height) {
+   const boundaries = [ 'gap', 'wall', 'door' ];
+
+   return [...Array(height).keys()].map((y) => {
+      return [...Array(width).keys()].map((x) => {
+         return {
+            N: y === 0 ? 'wall' : boundaries[prng.between(0, 2, true)],
+            W: x === 0 ? 'wall' : boundaries[prng.between(0, 2, true)],
+         };
+      });
+   });
+}
+
+function renderMap(map) {
+   const mapHeight = map.length,
+         mapWidth = map[0].length;
+
+   createFloor(mapHeight, mapWidth);
+   map.forEach((row, y) => {
+      row.forEach((cell, x) => {
+         createWall(x, y, 0, cell.N);
+         createWall(x, y, 3, cell.W);
+      });
+      createWall(mapWidth, y, 3, 'wall');
+   });
+
+   map[0].forEach((cell, x) => {
+      createWall(x, mapHeight, 0, 'wall');
+   });
+}
+
+window.onload = () => {
+   const seed = Math.floor(Math.random() * 100),
+         prng = PRNG(seed),
+         map = generateMap(prng, 10, 10);
+
+   console.log(seed);
+   renderMap(map);
+}
